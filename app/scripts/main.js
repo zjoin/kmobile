ba21b92d47be42155c6d7d28eb7494e5d3332911
Dima Valuev <document.letter@gmail.com>
2014-06-24 19:20:01 +0400

    new search

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -324,10 +324,71 @@ $('.navbar-toggle').on('click',function(){
     $(this).find('.rep').toggle();
 });
 
-$('body').on('click',function(){
+$('.container, .menu, .navbar-default, .col-xs-12').on('click',function(){
     if ($('body').hasClass('canvas-slid')) {
         $('.rep').hide();
  }
 });
 // end open arrow
+
+
+$('.masthead-search-toggle').on('click',function() {
+	 $('.masthead-search-indicator').toggleClass('bl-search');
+});
+
+$('.masthead-search-indicator').on('click',function() {
+	 $('.open-menu, .logo').hide();
+});
+
+
+
+
+
+
+
+	   function buttonUp(){
+         var valux = $('.sb-search-input').val();
+            valux = $.trim(valux).length;
+            if(valux !== 0){
+                $('.sb-search-submit').css('z-index','99');
+            } else{
+                $('.sb-search-input').val('');
+                $('.sb-search-submit').css('z-index','-999');
+            }
+    }
+
+    $(document).ready(function(){
+        var submitIcon = $('.sb-icon-search');
+        var submitInput = $('.sb-search-input');
+        var searchBox = $('.sb-search');
+        var isOpen = false;
+
+        $(document).mouseup(function(){
+            if(isOpen == true){
+            submitInput.val('');
+            $('.sb-search-submit').css('z-index','-999');
+            submitIcon.click();
+            }
+        });
+
+        submitIcon.mouseup(function(){
+            return false;
+        });
+
+        searchBox.mouseup(function(){
+            return false;
+        });
+
+        submitIcon.click(function(){
+            if(isOpen == false){
+                searchBox.addClass('sb-search-open');
+                isOpen = true;
+            } else {
+                searchBox.removeClass('sb-search-open');
+                isOpen = false;
+            }
+    });
+
+});
+
 
