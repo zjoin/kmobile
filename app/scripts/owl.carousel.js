eb2b17435c587a9baa3572eb4ac4a9f9cf105560
Dima Valuev <document.letter@gmail.com>
2014-06-30 19:12:15 +0400

    30 june add work

--- app/scripts/owl.carousel.js
+++ app/scripts/owl.carousel.js
@@ -1513,7 +1513,7 @@ if (typeof Object.create !== "function") {
 
         $(document).ready(function() {
 			$("#owl-demo").owlCarousel({
-      		autoPlay: 3000, //Set AutoPlay to 3 seconds
+      		autoPlay: 90000, //Set AutoPlay to 3 seconds
       		items : 4,
       		itemsDesktop : [1199,3],
       		itemsDesktopSmall : [979,3]
